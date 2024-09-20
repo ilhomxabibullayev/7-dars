12bf1a7b212538ee143facc043cf055e881c399e
ilhomxabibullayev <ilhomxabibullayev2004@gmail.com>
2024-09-20 08:56:53 +0500

    commit

--- js/app.js
+++ js/app.js
@@ -30,13 +30,40 @@ factorialize(5);
 
 // 4 masala
 
+const start = 2;
+const diff = 3;
+const n = 10;
+let sum = 0;
 
+for (let i = 0; i < n; i++) {
+    sum += start + i * diff;
+}
+
+console.log("Yig'indi:", sum);
+
+// 5 masala
+
+const input = prompt("Matn kiriting:");
+
+let result = "";
+
+for (let i = 0; i < input.length; i++) {
+    result += input[i].repeat(3);
+}
+
+console.log(result);
+
+// 6 masala
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i + " ning kvadrati: " + i * i);
+}
 
 // 7 masala
 
-let num = 12345;
-
-console.log(Number(num.toString().split('').reverse().join('')));
+for (let i = 20; i >= 1; i--) {
+    console.log(i);
+}
 
 // 8 masala
 
@@ -55,3 +82,16 @@ console.log(max(3, 7, 2, 10, 6));
 let text = "assalamu alaykum";
 
 console.log(text.length);
+
+// 10 masala
+
+numbers = [10, 20, 30, 40, 50];
+
+sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+let average = sum / numbers.length;
+
+console.log("O'rtacha qiymat:", average);
